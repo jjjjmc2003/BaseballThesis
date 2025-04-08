6c8c68f685b3422aaefbb9ccb24a318e25e99990
jjjjmc2003 <j.mcintosh25@ncf.edu>
2025-04-08 15:01:20 -0400

    Updated Chat

--- baseball_pages/chatbot.py
+++ baseball_pages/chatbot.py
@@ -22,7 +22,7 @@ def show():
     st.markdown("### 💬 Welcome to the **Baseball Stats Chatbot**")
     st.write("Ask anything about MLB hitters from **1950 to 2010** 📊⚾")
     st.write("**Note**: to ask about anything non baseball related or not related to the dataset prompt it using"
-               '"Using Knowledge Outside of the Dataset" will help it respond more accurately')
+               '\n"Using Knowledge Outside of the Dataset" will help it respond more accurately')
 
 
     # Chat History
