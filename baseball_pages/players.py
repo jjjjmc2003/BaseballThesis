462423e4474bd8c56b892c3729e2e1b127738980
jjjjmc2003 <j.mcintosh25@ncf.edu>
2025-04-15 14:29:51 -0400

    PLayers

--- baseball_pages/players.py
+++ baseball_pages/players.py
@@ -11,8 +11,7 @@ import seaborn as sns
 def show():
     st.title("Players (Power vs Contact)")
 
-    st.write("Note on the Player Names: * - bats left-handed, # - bats both (switch hitter), nothing - bats right,"
-               " ? - unknown")
+    st.write("Note on the Player Names: * - bats left-handed, # - bats both (switch hitter), nothing - bats right,")
 
     # Set up local data directory
     DATA_DIR = "data"
