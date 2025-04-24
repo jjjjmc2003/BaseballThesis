7a94307c9ead0ba3e9a51274af33518132745f9c
jjjjmc2003 <j.mcintosh25@ncf.edu>
2025-04-24 13:12:05 -0400

    dashboard

--- baseball_pages/dashboard.py
+++ baseball_pages/dashboard.py
@@ -4,7 +4,7 @@ def show():
     # Page Title
     st.title("⚾Hitting Evolution Dashboard⚾")
 
-    # Introduction Section
+    # Project overview
     st.markdown("## Project Overview")
     st.write(
         "This thesis investigates how Major League Baseball (MLB) hitters have changed from **1950 to the present day**, "
@@ -13,33 +13,36 @@ def show():
         "we compare decades of hitting data to examine whether the line between **power hitters** and **contact hitters** has blurred."
     )
 
-    st.markdown("## Research Goal")
+    st.markdown("## Research Goal") #Research Goals
     st.write(
         "The central aim is to **quantify how hitter profiles and offensive trends have evolved** over time and to determine the "
         "factors that may have driven those shifts. This includes investigating changes in performance, strategic approaches, and ."
     )
 
 
-    st.markdown("## Features")
+    st.markdown("## Features") #Features of project
     st.write(
-        "To explore these questions, we analyze MLB hitting data by decade using both statistical and machine learning techniques:"
+        "To explore these questions, we analyze MLB hitting data by decade using both statistical and"
+        " machine learning techniques:"
     )
-
+        #The features
     st.markdown(
         """
-        - **Clustering & Dimensionality Reduction**: Using **PCA** and **TSNE** to visualize and group hitters and decades by performance metrics
+        - **Clustering & Dimensionality Reduction**: Using **PCA** and **TSNE** to visualize and group
+         hitters and decades by performance metrics
         \n
-        - **Trend Analysis**: Observing decade-by-decade and yearly changes in **batting average**, **slugging percentage**, **strikeout rate**, etc
+        - **Trend Analysis**: Observing decade-by-decade and yearly changes in **batting average**, 
+        **slugging percentage**, **strikeout rate**, etc
         \n
-        - **Historical and Type Comparison**: Contrasting modern hitters with those from earlier decades to identify evolving archetypes, and contact vs power hitters
+        - **Historical and Type Comparison**: Contrasting modern hitters with those from earlier decades to
+         identify evolving archetypes, and contact vs power hitters
         \n
         - **Chatbot**: trained on dataset available to answer all questions baseball or completely unrelated 
         """
     )
 
 
-    st.markdown("## How to Explore?")
+    st.markdown("## How to Explore?") #Navigation directions
     st.write(
-        "Use the navigation menu to explore visual dashboards, watch how the TSNE's change over the years, and examine interactive analyses of how "
-        "MLB hitters have evolved"
+        "Use the navigation menu to the left to explore the web app"
     )
