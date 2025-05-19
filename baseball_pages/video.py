e7fc71c5bd21f48ce8f08d0a09bf8b34b4365690
jjjjmc2003 <j.mcintosh25@ncf.edu>
2025-05-18 21:27:18 -0400

    Corrected Text

--- baseball_pages/video.py
+++ baseball_pages/video.py
@@ -3,14 +3,7 @@ import streamlit as st
 
 # Page title
 def show():
-    st.title("Decade TSNE Plots (1950-2010)")
-
-    # Brief description
-    st.write(
-        "In this video the different TSNEs of hitters is displayed, the general idea of this section is to display"
-        "just how different the decades are from each other, some share similarities while others do not resemble"
-        "the other in the slightest "
-    )
+    st.title("Yearly TSNE Plots (1950-2010)")
 
     # YouTube video
     st.video("https://youtu.be/n8caodd2ZzA?si=70ldPUJfHY7cNaof")
